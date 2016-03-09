eefadcc9bc6890842ecfe100a602eb16fdd4e88c
kavipriyavijayakumar <kavipriya.v2012@kgkite.ac.in>
2016-03-09 16:10:18 +0530

    Added files via upload

--- patientreg.php
+++ patientreg.php
@@ -1,21 +1,29 @@
 <?php
-
-	
 	mysql_connect("localhost","root","");
 	mysql_select_db("doctor_appointment");
 	
-	$patient_name=$_REQUEST['PATIENT_NAME'];
-	$age=$_REQUEST['AGE'];
-        $gender=$_REQUEST['GENDER'];
-	$username=$_REQUEST['USERNAME'];
-        $pwd=$_REQUEST['PASSWORD'];
-	$confirm_pwd=$_REQUEST['CONFIRM_PWD'];
-	$ph_no=$_REQUEST['PHONE_NO'];
-	$email=$_REQUEST['EMAILID'];
-	$address=$_REQUEST['ADDRESS'];
+	$pname=$_REQUEST['PATIENT_NAME'];
+	$page=$_REQUEST['AGE'];
+	$pgender=$_REQUEST['GENDER'];
+        $pusername=$_REQUEST['USERNAME'];
+        $ppass=$_REQUEST['PASSWORD'];
+	$pconpass=$_REQUEST['CONFIRM_PWD'];
+	$pphno=$_REQUEST['PHONE_NO'];
+	$pemail=$_REQUEST['EMAIL_ID'];
+	$paddress=$_REQUEST['ADDRESS'];
 	
-    $SQL = "INSERT INTO 'patientreg'('PATIENT_NAME','AGE','GENDER','USERNAME','PASSWORD','CONFIRM_PWD','PHONE_NO','EMAILID','ADDRESS') VALUES ('".$patient_name."','".$age."','".$gender."','".$username."','".$pwd."','".$confirm_pwd."','".$ph_no."','".$email."','".$address."')";   
+$SQL = "INSERT INTO `patientreg`(`PATIENT_NAME`,`AGE`,`GENDER`,`USERNAME`,`PASSWORD`,`CONFIRM_PWD`,`PHONE_NO`,`EMAIL_ID`,`ADDRESS`) VALUES ('".$pname."','".$page."','".$pgender."','".$pusername."','".$ppass."',''".$pconpass."','".$pphno."','".$pemail."','".$paddress."')";   
 	$RES = mysql_query($SQL); 
+$sql1 = "select id from patientreg where PATIENT_NAME='".$pname."' ";
+
+$result=mysql_query($sql1);
+while ($row = mysql_fetch_array($result))
+{
+	$r=$row['id']; 
+
+
+}
+echo $r;
 	  
 	
 	
